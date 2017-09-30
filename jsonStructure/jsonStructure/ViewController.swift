0414fbaf0e65633a8ab54939e7fb6fcbcedb306a
Amin <amin@lemonorange.pl>
2017-09-30 22:10:51 +0200

    URL request JSON to Dictionary
    its working fine

--- jsonStructure/jsonStructure/ViewController.swift
+++ jsonStructure/jsonStructure/ViewController.swift
@@ -12,12 +12,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        let url = URL(string:"http://api.fixer.io/latest")
+        
+        let task  = URLSession.shared.dataTask(with: url!) { (data, respoce, error ) in
+            
+            if error != nil
+            {
+                print("Something ERROR!")
+            }
+            else
+            {
+                if let info = data
+                {
+                    do
+                    {
+                        let myJSON = try JSONSerialization.jsonObject(with: info, options: JSONSerialization.ReadingOptions.mutableLeaves) as AnyObject
+                        
+                       if let values = myJSON["rates"] as? NSDictionary
+                       {
+                       // print(values);
+                        
+                        print(values["USD"]!);
+                       }
+                    }
+                    catch {}
+                }
+            }
+            
+        }
+        task.resume()
     }
 
 
